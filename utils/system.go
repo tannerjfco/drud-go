946cabb4c7f58deca73566dc946d2cafac424ad9
Erin Corson <frodopwns@gmail.com>
2016-09-27 14:22:15 -0600

    formatting was off

--- utils/system.go
+++ utils/system.go
@@ -39,10 +39,10 @@ func RunCommand(command string, args []string) (string, error) {
 
 // FileExists checks a file's existence
 func FileExists(name string) bool {
-    if _, err := os.Stat(name); err != nil {
-    if os.IsNotExist(err) {
-                return false
-            }
-    }
-    return true
+	if _, err := os.Stat(name); err != nil {
+		if os.IsNotExist(err) {
+			return false
+		}
+	}
+	return true
 }
